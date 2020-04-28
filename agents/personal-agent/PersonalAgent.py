333b7e1985ddc024f72707540ab47db7a1b2beeb
piotr-onyszczuk <piotr.onyszczuk1@gmail.com>
2020-04-28 15:06:45 -0400

    Adjust agents to new documentation

--- agents/personal-agent/PersonalAgent.py
+++ agents/personal-agent/PersonalAgent.py
@@ -4,26 +4,125 @@ from spade.message import Message
 from spade.template import Template
 from spade import quit_spade
 import json
+import uuid
 
 
 class PersonalAgent(Agent):
+
     @staticmethod
-    def prepare_meet_request(self, date, temperature, receivers):
+    def prepare_meet_request(self, guid, start_date, end_date, temperature, participants ,receivers):
         msg = Message(to=receivers)
         msg.set_metadata('performative', 'request')
         msg.set_metadata('type', 'meet')
-        msg.body = json.dumps({'date': date, 'temperature': temperature})
+        msg.body = json.dumps({'meeting_guid': guid, 'start_date': date, 'end_date': end_date, 'temperature': temperature, 'participants':participants})
+        return msg
+
+    @staticmethod
+    def prepare_late_inform(self, arrival_datetime ,receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'inform')
+        msg.set_metadata('type', 'late')
+        msg.body = json.dumps({'arrival_datetime': arrival_datetime})
+        return msg
+
+    @staticmethod
+    def prepare_preferences_inform(self, optimal_temperature ,receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'inform')
+        msg.set_metadata('type', 'preferences')
+        msg.body = json.dumps({'optimal_temperature': optimal_temperature})
+        return msg
+
+    @staticmethod
+    def prepare_accept_proposal(self, guid ,receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'accept_proposal')
+        msg.set_metadata('type', 'accept_proposal')
+        msg.body = json.dumps({'meeting_guid': guid})
+        return msg
+
+    @staticmethod
+    def prepare_refuse_proposal(self, guid ,receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'refuse_proposal')
+        msg.set_metadata('type', 'refuse_proposal')
+        msg.body = json.dumps({'meeting_guid': guid})
         return msg
     
-    class SendReqBehav(CyclicBehaviour):
+    meet_inform_template = Template()
+    meet_inform_template.set_metadata('performative','inform')
+    meet_inform_template.set_metadata('type','meet')
+
+    new_meeting_inform_template = Template()
+    new_meeting_inform_template.set_metadata('performative','inform')
+    new_meeting_inform_template.set_metadata('type','new_meeting')
+
+    late_confirm_template = Template()
+    late_confirm_template.set_metadata('performative','confirm')
+    late_confirm_template.set_metadata('type','late')
+
+    datetime_inform_template = Template()
+    datetime_inform_template.set_metadata('performative','inform')
+    datetime_inform_template.set_metadata('type','datetime')
+
+    move_meeting_propose_template = Template()
+    move_meeting_propose_template.set_metadata('performative','propose')
+    move_meeting_propose_template.set_metadata('type','move_meeting')
+
+    move_meeting_inform_template = Template()
+    move_meeting_inform_template.set_metadata('performative','inform')
+    move_meeting_inform_template.set_metadata('type','move_meeting')
+
+    class SendMeetRequestBehaviour(CyclicBehaviour):
 	async def run(self):
-	    msg = PersonalAgent.prepare_meet_request(self,'jakas data',20,'central_agent')
+	    msg = PersonalAgent.prepare_meet_request(self, uuid.uuid4(), 'start_date','end_date',20,['AA@AA', 'bb@bb'],'central_agent')
 	    await self.send(msg)
-	    template = Template()
-	    template.set_metadata('performative','inform')
-	    template.set_metadata('type','meet')
-	    #tu czekanie na template
+	    #tu czekanie na template meet_inform
+
+    class SendLateInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = PersonalAgent.prepare_late_inform(self, 'arrival_date', 'central_agent')
+	    await self.send(msg)
+	    #tu czekanie na template late_confirm
+
+    class ReceiveNewMeetingInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
+
+    class ReceiveDatetimeInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
+
+    class SendPreferencesInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = PersonalAgent.prepare_preferences_inform(self, 'arrival_date', 'private_room_agent')
+	    await self.send(msg)
+
+    class ReceiveMoveMeetingProposeBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
+	    # wyslane accept lub refuse
+
+    class ReceiveMoveMeetingInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
 
     async def setup(self)
-	sending = self.SendReqBehav()
-	self.addbehaviour(sending)
+	sending = self.SendMeetRequestBehaviour()
+	self.add_behaviour(sending)
+	late_inform = self.SendLateInformBehaviour()
+	self.add_behaviour(late_inform)
+        new_meeting = self.ReceiveNewMeetingInformBehaviour()
+	self.add_behaviour(new_meeting,new_meeting_inform_template)
+        datetime_inform = self.ReceiveDatetimeInformBehaviour()
+	self.add_behaviour(datetime_inform,datetime_inform_template)
+	preferences = self.SendPreferencesInformBehaviour()
+	self.add_behaviour(preferences)
+	move_meeting_propose = self.ReceiveMoveMeetingProposeBehaviour()
+	self.add_behaviour(move_meeting_propose,move_meeting_propose_template)
+	move_meeting_inform = self.ReceiveMoveMeetingInformBehaviour()
+	self.add_behaviour(move_meeting_inform,move_meeting_inform_template)
---
+++ agents/private-room-agent/PrivateRoomAgent.py
@@ -0,0 +1,110 @@
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+from spade.message import Message
+from spade.template import Template
+from spade import quit_spade
+import json
+
+
+class PrivateRoomAgent(Agent):
+
+    @staticmethod
+    def prepare_room_data_exchange_request(self, temperature, receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'request')
+        msg.set_metadata('type', 'room_data_exchange')
+        msg.body = json.dumps({'temperature': temperature})
+        return msg
+
+    @staticmethod
+    def prepare_room_data_inform(self, temperature, receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'inform')
+        msg.set_metadata('type', 'room_data')
+	msg.body = json.dumps({'temperature': temperature})
+        return msg
+
+    @staticmethod
+    def prepare_energy_usage_inform(self, energy, receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'inform')
+        msg.set_metadata('type', 'energy_usage')
+        msg.body = json.dumps({'energy_used_since_last_message': energy})
+        return msg
+
+    @staticmethod
+    def prepare_outdoor_temperature_request(self, receivers):
+        msg = Message(to=receivers)
+        msg.set_metadata('performative', 'request')
+        msg.set_metadata('type', 'outdoor_temperature')
+        msg.body = json.dumps({})
+        return msg
+
+    room_data_exchange_request_template = Template()
+    room_data_exchange_request_template.set_metadata('performative','request')
+    room_data_exchange_request_template.set_metadata('type','room_data_exchange')
+
+    room_data_inform_template = Template()
+    room_data_inform_template.set_metadata('performative','inform')
+    room_data_inform_template.set_metadata('type','room_data')
+
+    datetime_inform_template = Template()
+    datetime_inform_template.set_metadata('performative','inform')
+    datetime_inform_template.set_metadata('type','datetime')
+
+    outdoor_temperature_inform_template = Template()
+    outdoor_temperature_inform_template.set_metadata('performative','inform')
+    outdoor_temperature_inform_template.set_metadata('type','outdoor_temperature')
+
+    preferences_inform_template = Template()
+    preferences_inform_template.set_metadata('performative','inform')
+    preferences_inform_template.set_metadata('type','preferences')
+
+
+    class ReceiveRoomDataExchangeRequestBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
+	    #wyslanie room_data_inform
+
+    class SendRoomDataExchangeRequestBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = MeetingRoomAgent.prepare_room_data_exchange_request(self, 20, 'another_room_agent')
+	    await self.send(msg)
+
+    class ReceiveDatetimeInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
+
+    class SendEnergyUsageInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = MeetingRoomAgent.prepare_energy_usage_inform(self, 20, 'energy_agent')
+	    await self.send(msg)
+
+    class SendOutdoorTemperatureRequestBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = MeetingRoomAgent.prepare_outdoor_temperature_request(self, 'outdoor_agent')
+	    await self.send(msg)
+	    #czekanie na inform
+
+    class ReceivePreferencesInformBehaviour(CyclicBehaviour):
+	async def run(self):
+	    msg = await self.receive()
+	    msg_data = json.loads(msg.body)
+
+
+    async def setup(self)
+	receive_room_data_request = self.ReceiveRoomDataExchangeRequestBehaviour()
+	self.add_behaviour(room_data,room_data_exchange_request_template)
+	send_room_data_request = self.SendRoomDataExchangeRequestBehaviour()
+	self.add_behaviour(send_room_data_request)
+        datetime_inform = self.ReceiveDatetimeInformBehaviour()
+	self.add_behaviour(datetime_inform,datetime_inform_template)
+	energy_usage = self.SendEnergyUsageInformBehaviour()
+	self.add_behaviour(energy_usage)
+	outdoor_temperature = self.SendOutdoorTemperatureRequestBehaviour()
+	self.add_behaviour(outdoor_temperature)
+	preferences = self.ReceivePreferencesInformBehaviour()
+	self.add_behaviour(preferences,preferences_inform_template)
+
