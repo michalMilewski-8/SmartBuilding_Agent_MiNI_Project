6f8d93fc47f67a3eea207f06d9f8d0723c435ace
Jakub Drak Sbahi <jakubdrak98@gmail.com>
2020-04-29 23:50:33 +0200

    Refactor code

--- agents/thermometer-agent/thermometer.py
+++ agents/thermometer-agent/thermometer.py
@@ -3,24 +3,27 @@ import pandas as pd
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
 from spade.message import Message
+from spade.template import Template
 
-temp = pd.read_csv('EPWA.01.01.2019.31.12.2019.1.0.0.en.ansi.00000000.csv',
-    parse_dates=['Local time in Warsaw / Okecie (airport)'])
-print(temp.head())
-print(type(temp['Local time in Warsaw / Okecie (airport)'][1]))
-print(temp['Local time in Warsaw / Okecie (airport)'][1])
+temp_data = pd.read_csv(
+    'agents/thermometer-agent/weather2019warsaw.csv',
+    parse_dates=['Local time in Warsaw / Okecie (airport)'],
+    index_col='Local time in Warsaw / Okecie (airport)')  
 
 class Thermometer(Agent):
-    def setup(self):        
-        b = self.MyBehav()
+    def setup(self):       
+        template = Template()
+        template.set_metadata('performative', 'request')
+        template.set_metadata('type', 'outdoor_temperature')
+        b = self.SendTemperature()
         self.add_behaviour(b)
 
-    class MyBehav(CyclicBehaviour):
+    class SendTemperature(CyclicBehaviour):
         async def run(self):
             msg = await self.receive()
             date_dict = json.loads(msg.body)
             date = date_dict['date']
-            temp = Thermometer.get_temperature(pd.Timestamp(date))
+            temp = self.agent.get_temperature(pd.Timestamp(date))
 
             temp_msg = msg.make_reply()
             msg.body = json.dumps({'temperature': temp})
@@ -29,13 +32,7 @@ class Thermometer(Agent):
             await self.send(temp_msg)
 
 
-
-    @staticmethod
-    def get_temperature(date):   
-        temp_data = pd.read_csv(
-            'EPWA.01.01.2019.31.12.2019.1.0.0.en.ansi.00000000.csv',
-            parse_dates=['Local time in Warsaw / Okecie (airport)'],
-            index_col='Local time in Warsaw / Okecie (airport)')     
+    def get_temperature(self, date):       
         date = date.round('30min')
         date = pd.Timestamp(year=2019, month=date.month, day=date.day,
             hour=date.hour, minute=date.minute)
